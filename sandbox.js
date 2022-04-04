7ec34c02433911838a107e56b4d3fcc8fd4ee6f9
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-04-04 11:00:31 -0700

    added title case

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,15 @@
+function titleCase(sentence) {
+  let lowerCase = sentence
+    .toLowerCase()
+    .split(' ')
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+  return lowerCase;
+}
+console.log(titleCase('hello every body'));
+
 function at(arr, index) {
   if (index < 0) {
     return arr[arr.length + index];
